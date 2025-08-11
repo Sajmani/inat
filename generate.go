bad4a277179abd6be5a13326ccc1dd1370ff06ba
Sameer Ajmani <sameer@google.com>
2025-08-11 10:28:32 -0400

    Add package doc comment.

--- generate.go
+++ generate.go
@@ -1,3 +1,4 @@
+// Package inat contains a Go client for the iNaturalist API generated using oapi-codegen.
 package inat
 
 //go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen -config cfg.yaml api.json
